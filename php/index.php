0cfdf78a1b8b2ed5584aa8930ee861bc53fad471
David Petr <petrhk.david@gmail.com>
2024-11-05 17:23:17 +0100

    Vytvoření assets>header.php; css>general,header;   základní struktura header + malé stylování

--- php/index.php
+++ php/index.php
@@ -1,3 +1,20 @@
-<?php
-    echo "Hello Tour de App";
-?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <link rel="stylesheet" href="./css/general/style.css">
+    <link rel="stylesheet" href="./css/header/style.css">
+
+    <link rel="shortcut icon" href="./img/Think-different-Academy_LOGO_erb.png" type="image/x-png">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Think different Academy</title>
+</head>
+<body>
+    <?php require "./assets/header.php" ?>
+    
+    <?php
+        echo "Hello Tour de App";
+    ?>
+</body>
+</html>
